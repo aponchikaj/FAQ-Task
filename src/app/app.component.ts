2d593e9f5de402d8060fb1414c13d8b5f4fc17e2
Lazare Mirziashvili <lazzexa@gmail.com>
2024-05-02 23:54:33 +0400

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'App';
+  FirstQuestion:boolean = false;
+  SecondQuestion:boolean = false;
+  ThirdQuestion:boolean = false;
+  FourthQuestion:boolean = false;
+
+  Plus:boolean = true;
+
+  OpenFirstQuestion(){
+    this.Plus = false;
+    this.FirstQuestion = true;
+  }
+
+  CloseFirstQuestion(){
+    this.Plus = true;
+    this.FirstQuestion = false;
+  }
+  OpenSecondQuestion(){
+    this.Plus = false;
+    this.SecondQuestion = true;
+  }
+
+  CloseSecondQuestion(){
+    this.Plus = true;
+    this.SecondQuestion = false;
+  }
+
+  OpenThirdQuestion(){
+    this.Plus = false;
+    this.ThirdQuestion = true;
+  }
+
+  CloseThirdQuestion(){
+    this.Plus = true;
+    this.ThirdQuestion = false;
+  }
+
+  OpenFourthQuestion(){
+    this.Plus = false;
+    this.FourthQuestion = true;
+  }
+
+  CloseFourthQuestion(){
+    this.Plus = true;
+    this.FourthQuestion = false;
+  }
 }
